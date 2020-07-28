fbc7fa237a0e042220e7a2723c55ec82c1654178
Akansha0211 <sanjaysxn25@gmail.com>
2020-07-28 11:05:27 +0530

    Assignement11.2 complete

--- Assignement11.2.py
+++ Assignement11.2.py
@@ -6,8 +6,31 @@
 
 
 import re
-# name = input('Enter file name')
-fhandle = open('Data_For_Assignement11.2')
-number = list()
-for line in fhandle:
-    number = re.findall('[0-9]',line)
+fh = open('Data_For_Assignement11.2')
+sum = 0
+lst = list()
+for line in fh:
+    line = line.rstrip()
+    y = re.findall('[0-9]+',line)
+    lst = lst + y
+print(lst)
+for element in lst:
+    sum = sum + int(element)
+print(sum)
+
+
+# Optional: Just for Fun
+#
+# There are a number of different ways to approach this problem. While we don't recommend trying to write the most compact code possible, it can sometimes be a fun exercise. Here is a a redacted version of two-line version of this program using list comprehension:
+#
+# Python 2
+# import re
+# print sum( [ ****** *** * in **********('[0-9]+',**************************.read()) ] )
+#
+# Python 3:
+# import re
+# print( sum( [ ****** *** * in **********('[0-9]+',**************************.read()) ] ) )
+#
+# Please don't waste a lot of time trying to figure out the shortest solution until you have completed the homework. List comprehension is mentioned in Chapter 10 and the read() method is covered in Chapter 7.
+#
+#
